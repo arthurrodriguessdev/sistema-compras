228a4a1b0d7e676b688c6e3f55b9ec4bc247fdb8
Arthur <arthurdev.contatoo@gmail.com>
2025-05-13 13:38:43 -0300

    'ClassePagamentoFinalizada'

--- CarrinhoDeCompras.java
+++ CarrinhoDeCompras.java
@@ -1,5 +1,12 @@
 package SistemaDeComprasVendas;
 
-public class CarrinhoDeCompras {
+public class CarrinhoDeCompras { //Classe molde Carrinho de compras
+	
+	//Definição de atributos
+	private String listaProdutos;
+	private double totalCompra;
+	
+	//Definição de métodos
+	
 
 }
--- Pagamento.java
+++ Pagamento.java
@@ -1,5 +1,37 @@
 package SistemaDeComprasVendas;
 
-public class Pagamento {
+public class Pagamento { //Classe molde do pagamento
+	
+	//Definição de atributos
+	private double valorTotal;
+	private String statusPagamento;
+	
+	//Definição de métodos
+	public String realizarPagamento(String statusPagamento) {
+		statusPagamento = "Pago";
+		return "Pagamento realizado com sucesso!";
+	}
+	
+	public String cancelarPagamento() {
+		statusPagamento = "Pagamento pendente";
+		return statusPagamento;
+	}
+	
+	public void exibirStatusPagamento() {
+		System.out.println(statusPagamento);
+	}
+	
+	public void exibirValorTotal() {
+		System.out.println(valorTotal);
+	}
+	
+	//Definição de GET e SET
+	public String getExibirStatusPagamento() {
+		return statusPagamento;
+	}
+	
+	public double getExibirValorTotal() {
+		return valorTotal;
+	}
 
 }
